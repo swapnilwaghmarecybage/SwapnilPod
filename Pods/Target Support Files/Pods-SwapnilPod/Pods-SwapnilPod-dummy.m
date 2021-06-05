9ae584600014121b184e979661e0ddb98bceff75
swapnilwaghmarecybage <swapnilwaghm@cybage.com>
2021-06-05 21:40:33 +0530

    Initial Commit

---
+++ Pods/Target Support Files/Pods-SwapnilPod/Pods-SwapnilPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwapnilPod : NSObject
+@end
+@implementation PodsDummy_Pods_SwapnilPod
+@end
---
+++ SwapnilPod/SwapnilPod.h
@@ -0,0 +1,18 @@
+//
+//  SwapnilPod.h
+//  SwapnilPod
+//
+//  Created by Swapnil Waghmare on 05/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwapnilPod.
+FOUNDATION_EXPORT double SwapnilPodVersionNumber;
+
+//! Project version string for SwapnilPod.
+FOUNDATION_EXPORT const unsigned char SwapnilPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwapnilPod/PublicHeader.h>
+
+
